d5df571cc66e007fd3d08e13fc0fed6e5c6b057f
Tom Borger <thomas.borger@gmail.com>
2014-01-28 16:41:05 -0600

    Register post types

--- class-mia-author.php
+++ class-mia-author.php
@@ -74,7 +74,7 @@ class MIA_Author{
 	 * @param string $name The name of the collection to remove.
 	 * @return bool Returns true on success, WP_Error on failure.
 	 */
-	function unregister_collection( $name ){}
+	function unregister_collection( $name ) {}
 
 
 	/**
@@ -84,7 +84,7 @@ class MIA_Author{
 	 * 
 	 * @return array Returns a copy of the $collections property
 	 */
-	function get_collections(){}
+	function get_collections() {}
 
 
 	/**
@@ -94,7 +94,101 @@ class MIA_Author{
 	 * 
 	 * @return bool Returns true if collection is registered, otherwise false.
 	 */
-	function collection_exists(){}
+	function collection_exists() {}
+
+
+	/**
+	 * Register Object custom post type.
+	 *
+	 * @since 0.0.1
+	 */
+	function register_object_cpt() {
+
+		$labels = array(
+			'name'                => _x( 'Objects', 'Post Type General Name', 'mia-author' ),
+			'singular_name'       => _x( 'Object', 'Post Type Singular Name', 'mia-author' ),
+			'menu_name'           => _x( 'Objects', 'Post Type Menu Name', 'mia-author' ),
+			'parent_item_colon'   => __( 'Parent Object:', 'mia-author' ),
+			'all_items'           => __( 'All Objects', 'mia-author' ),
+			'view_item'           => __( 'View Object', 'mia-author' ),
+			'add_new_item'        => __( 'Add New Object', 'mia-author' ),
+			'add_new'             => __( 'Add New', 'mia-author' ),
+			'edit_item'           => __( 'Edit Object', 'mia-author' ),
+			'update_item'         => __( 'Update Object', 'mia-author' ),
+			'search_items'        => __( 'Search Objects', 'mia-author' ),
+			'not_found'           => __( 'Not found', 'mia-author' ),
+			'not_found_in_trash'  => __( 'Not found in Trash', 'mia-author' ),
+		);
+
+		$args = array(
+			'label'               => __( 'object', 'mia-author' ),
+			'description'         => __( 'Represents a primary record in the application.', 'mia-author' ),
+			'labels'              => $labels,
+			'supports'            => array( 'title', 'editor', ),
+			'hierarchical'        => false,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'show_in_nav_menus'   => false,
+			'show_in_admin_bar'   => true,
+			'menu_position'       => 5,
+			'can_export'          => true,
+			'has_archive'         => false,
+			'exclude_from_search' => true,
+			'publicly_queryable'  => true,
+			'capability_type'     => 'post',
+		);
+
+		register_post_type( 'object', $args );
+
+	}
+
+
+	/**
+	 * Register Story custom post type.
+	 *
+	 * @since 0.0.1
+	 */
+	function register_story_cpt() {
+
+		$labels = array(
+			'name'                => _x( 'Stories', 'Post Type General Name', 'mia-author' ),
+			'singular_name'       => _x( 'Story', 'Post Type Singular Name', 'mia-author' ),
+			'menu_name'           => _x( 'Stories', 'Post Type Menu Name', 'mia-author' ),
+			'parent_item_colon'   => __( 'Parent Story:', 'mia-author' ),
+			'all_items'           => __( 'All Stories', 'mia-author' ),
+			'view_item'           => __( 'View Story', 'mia-author' ),
+			'add_new_item'        => __( 'Add New Story', 'mia-author' ),
+			'add_new'             => __( 'Add New', 'mia-author' ),
+			'edit_item'           => __( 'Edit Story', 'mia-author' ),
+			'update_item'         => __( 'Update Story', 'mia-author' ),
+			'search_items'        => __( 'Search Stories', 'mia-author' ),
+			'not_found'           => __( 'Not found', 'mia-author' ),
+			'not_found_in_trash'  => __( 'Not found in Trash', 'mia-author' ),
+		);
+
+		$args = array(
+			'label'               => __( 'story', 'mia-author' ),
+			'description'         => __( 'Represents secondary media related to a primary record in the application.', 'mia-author' ),
+			'labels'              => $labels,
+			'supports'            => array( 'title', 'editor', ),
+			'hierarchical'        => false,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'show_in_nav_menus'   => false,
+			'show_in_admin_bar'   => true,
+			'menu_position'       => 5,
+			'can_export'          => true,
+			'has_archive'         => false,
+			'exclude_from_search' => true,
+			'publicly_queryable'  => true,
+			'capability_type'     => 'post',
+		);
+
+		register_post_type( 'story', $args );
+
+	}
 
 
 	/**
@@ -102,7 +196,7 @@ class MIA_Author{
 	 *
 	 * @since 0.0.1
 	 */
-	function enqueue_scripts(){ 
+	function enqueue_scripts() { 
 
 		// Angular core
 		wp_enqueue_script( 
@@ -133,7 +227,7 @@ class MIA_Author{
 	 *
 	 * @since 0.0.1
 	 */
-	function print_fields(){
+	function print_fields() {
 
 		// TODO: Prepare field data
 
@@ -154,6 +248,10 @@ class MIA_Author{
 	 * @param bool $load_default If true, load default fields from plugin.
 	 */
 	function __construct( $load_default = true ) {
+
+		// Register Object and Story post types
+		add_action( 'init', array( $this, 'register_object_cpt' ) );
+		add_action( 'init', array( $this, 'register_story_cpt' ) );
 
 		// Queue scripts and field data to be processed and printed at page load
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
